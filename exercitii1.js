f48c0d3ccdc3eefe41551ac71539cffd69afee04
Zethyne <stefanolarnic@gmail.com>
2023-01-12 14:40:25 +0200

    first commit

---
+++ exercitii1.js
@@ -0,0 +1,25 @@
+// Exercitiul 1
+
+const rezultat = (2 + 3 * 4 - 5);
+console.log("Rezultat: ", rezultat);
+
+// Exercitiul 2
+
+const rezultat2 = 4 * (2 + 6/3);
+console.log("Rezultat: ", rezultat2);
+
+// Exercitiul 3
+
+const rezultat3 = 2 * 3**2 + 2**5
+console.log("Rezultat: ", rezultat3);
+
+// Exercitiul 4
+
+const rezultat4 = (5 * (3 + 2/2)) + 3
+console.log("Rezultat: ", rezultat4);
+
+// Exercitiul 5
+
+const sum5 = 2 ** 5 - 5 ** 2 + 3 * (5 - 4 / 2);
+const sum6 = (2 ** 5 + 5 ** 2);
+console.log(sum5 / sum6);
---
+++ exercitii2.js
@@ -0,0 +1,10 @@
+// Problema 2
+
+let x = 5;
+let y = 8;
+
+const sum1 = 2 * x - 5 * y
+console.log("Rezultat: ", sum1);
+
+const sum2 = 3 * x + 9 * y
+console.log("Rezultat: ", sum2);
